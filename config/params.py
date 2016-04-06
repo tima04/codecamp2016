bfd880941f4e962b1ce1ec390448a268cc4812e7
Paul Bodily <norkish@gmail.com>
2016-04-06 17:26:47 +0200

    add params.py constants

---
+++ config/params.py
@@ -0,0 +1,7 @@
+NOC_DATA_DIR = "../include/data/"
+SCEALEXTRIC_DIR = "../include/Scealextric/DATA/"
+
+NOC_FILEPATH = "Veale's The NOC List.xlsx"
+SCRIPT_MIDPOINTS_FILEPATH = "Veale's script midpoints.xlsx"
+INITIAL_BOOKENDS_FILEPATH = "Veale's initial bookend actions.xlsx"
+CLOSING_BOOKENDS_FILEPATH = "Veale's closing bookend actions.xlsx"
