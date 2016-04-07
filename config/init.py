44cba6d6125066dce1e5a7ee0fb038a73aa92e20
Samarpan <samarpan@localhost.aup>
2016-04-07 09:12:29 +0200

    Removing print statements

--- config/init.py
+++ config/init.py
@@ -1,12 +1,2 @@
-# from params .py import *
-# print 'Noc-vaele directory'
-# print params.DATA_NOC_VEALE
-# print 'DATA_NOC_TSV_LISTS'
-# print params.DATA_NOC_TSV_LISTS
-# print 'DATA_SCEALEXTRIC'
-# print params.DATA_SCEALEXTRIC
 from params import DATA_NOC_VEALE_LISTS,DATA_NOC_TSV_LISTS,DATA_SCEALEXTRIC_LISTS 
-print DATA_NOC_VEALE_LISTS
-print DATA_NOC_TSV_LISTS
-print DATA_SCEALEXTRIC_LISTS
 
